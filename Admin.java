abde2815fcda6e89bd1ff754acac55a73521477d
RowyMonke <nattakritta_ao@cmu.ac.th>
2025-07-24 11:55:38 +0700

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public class Admin extends User {
+    public Admin(String name, int year, int month, int dateOfMonth) {
+        super(name, year, month, dateOfMonth); //constructor use as multiple
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("User type: admin");
+    }
+    public void displayInfo(boolean full) {
+        if(full) {
+            this.displayInfo();
+            System.out.println("Today's date: " + LocalDate.now());
+        } else {
+            System.out.println(this.getName());
+        }
+    }
+}
